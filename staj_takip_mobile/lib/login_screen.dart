3460998790fb07563a141046c01baba2eadb40ca
Tuna Kılıç <tk05412527141@outlook.com>
2026-02-18 23:35:58 +0300

    .

--- staj_takip_mobile/lib/login_screen.dart
+++ staj_takip_mobile/lib/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'signup_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
